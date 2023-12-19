a91f5565c856bb68d331bf94c41656e4ae19e918
Amir-Muradov <amirmuradov0631@gmail.com>
2023-12-19 10:02:47 +0400

    sevenup

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+// var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+// var password = "";
+// for (var i = 0; i < 6; i++) {
+//     var randomIndex = Math.floor(Math.random() *62 );
+//     password += alphabet.charAt(randomIndex);
+// }
+// console.log("", password);
+
+
+
+
+// var sentence = "Kuznets KhNUE Personal Learning Systems";
+// var words = sentence.split(" ");    
+
+
+
+
+// var sentence = prompt("Write the sentence");
+// var bigword = sentence.toUpperCase();
+// console.log("", bigword);
+
+
+
+
+// var sentence = prompt("Write the sentence");
+// var smallword = sentence.toLowerCase();
+// console.log("", smallword);
+
+
+
+
+// var sentence = prompt("Write the sentence");
+// var bigword = sentence.charAt(0).toUpperCase() + sentence.substr(1);
+// console.log("", bigword);
+
+
+
+
+// var sentence = prompt("Write the sentence");
+// var smallword = sentence.substr(0, -1) + sentence.substr(-1).toUpperCase();
+// console.log("", smallword);
+
